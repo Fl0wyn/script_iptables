fd40d62d9eae7d5ca9a88e5c650110a33973f586
debmus <50771957+debmus@users.noreply.github.com>
2020-06-01 00:53:27 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-######################
-## FILTRAGE DE BASE ##
-######################
+#####################
+## BASIC FILTERING ##
+#####################
 
-# Vider les règles déjà existantes
+# Empty existing rules
 iptables -F
 iptables -X
 
-# Refuser toutes les connexions
+# Deny all connections
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
-# Autoriser le trafic interne sur l'adresse de loopback
+# Allow internal traffic on the loopback address
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Autoriser une connexions déjà établie
+# Allow an already established connection
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-####################
-## FILTRAGE PERSO ##
-####################
+######################
+## CUSTOM FILTERING ##
+######################
 
 # SSH
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -61,11 +61,11 @@ iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
-#####################
-## FILTRAGE AVANCÉ ##
-#####################
+########################
+## ADVANCED FILTERING ##
+########################
 
-# Autoriser les types de paquets IMCP utiles
+# Allow useful IMCP packet types
 iptables -A OUTPUT -p icmp -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type source-quench -j ACCEPT
@@ -73,29 +73,23 @@ iptables -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type parameter-problem -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
-# Empêcher les inondations de ping
+# Preventing ping floods
 iptables -N ICMPFLOOD
 iptables -A ICMPFLOOD -m recent --name ICMP --set --rsource
 iptables -A ICMPFLOOD -m recent --name ICMP --update --seconds 1 --hitcount 6 --rsource --rttl -m limit --limit 1/sec --limit-burst 1 -j LOG --log-prefix "iptables[ICMP-flood]: "
 iptables -A ICMPFLOOD -m recent --name ICMP --update --seconds 1 --hitcount 6 --rsource --rttl -j DROP
 iptables -A ICMPFLOOD -j ACCEPT
 
-# Supprimez les paquets non conformes
+# Delete packages invalid / NULL / syn-flood / XMAS
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-
-# Supprimer tous les paquets NULL mal formés entrants
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-
-# Supprimer les paquets d'attaque syn-flood
 iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
-
-# Supprimer les paquets XMAS mal formés entrants
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
-# IP à bannir depuis le fichier "banlist.txt"
-cat banip.txt | while read line ; do iptables -A INPUT -s $line -j DROP ; done
+# IP to ban from the file "banlist.txt"
+##cat banip.txt | while read line ; do iptables -A INPUT -s $line -j DROP ; done
 
-# Liste des IP à bannir avec ipset
+# List of IPs to ban with ipset
 ipset -q flush ipsum
 ipset -q create ipsum hash:net
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1); do ipset add ipsum $ip; done
